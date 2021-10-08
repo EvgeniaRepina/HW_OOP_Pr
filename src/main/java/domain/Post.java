ab3ad74c7f973f863db023432cd1cd5809b2131e
EvgeniaRepina <88374316+EvgeniaRepina@users.noreply.github.com>
2021-10-08 12:21:53 +0200

    v1

---
+++ src/main/java/domain/Post.java
@@ -0,0 +1,15 @@
+package domain;
+
+public class Post {
+    private String id;
+    private String date;
+    private String content;
+    private String imageUrl;
+    private String heading;
+    private int likes;
+    private String comments;
+    private int commentsNumber;
+    private int views;
+
+// геттеры и сеттеры
+}
